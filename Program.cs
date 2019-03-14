f8eee177568341bc9d4aae9a4e9667b6b5684291
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-14 15:01:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                    if (i == num)
+                        Console.WriteLine("This is Prime!");
+                    else
+                    {
+                        Console.WriteLine("This is NOT a prime!");
+                        break;
+                    }
+                        
+            }       
+        }
+    }
+}
